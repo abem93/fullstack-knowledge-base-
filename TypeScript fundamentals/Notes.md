a56190e314df8b4f0f04b142fea318d319f358a6
Abraham Mejia <aimejia4@gmail.com>
2023-09-25 06:31:44 -0500

    Union Types, Type Aliases, Function Types, Generics, Class structures, and Interfaces completed

--- TypeScript fundamentals/Notes.md
+++ TypeScript fundamentals/Notes.md
@@ -9,3 +9,8 @@
 
 # Base Types, Primitives, Arrays, Object Types, and Type Inference
   - Primitives are the core value types those include numbers strings and booleans and those come from JS. Arrays and objects considered a more complex type of type of assignment. Type inference referes to when you declare a variable, what ever type you assign it that is the type that is inferred.
+
+# Union Types, Type Aliases, Function Types, Generics, Class structures, and Interfaces.
+  - Union types allow for the type of the variable to accept multiples types that you input or even "any" type but that is less recommended. It is more common to define the specific inputs you expect. Function types infers the return value of the function based on the input, or it can be set manually out side the parameters parantheses. Funtions that do not have return value, are inferred as void, basically null or undefined. Generic types allow functions to to look in side arrays and based on whats inside, out put a value of the same kind. It infers the return value is the same value as the input value and will catch errors if they do not match. Classes allow us to define properties with all the types before hand. Class properties can be defined as private or public. Interfaces feature allows your class to have a certain structure. 
+
+# Configuring the TypeScript Compiler.
--- TypeScript/basics.ts
+++ TypeScript/basics.ts
@@ -10,7 +10,7 @@ let age: number = 24;
 
 age = 12;
 
-let userName: string;
+let userName: string | string [];
 userName = 'Max';
 
 let isInstructor: boolean;
@@ -24,10 +24,17 @@ let hobbies: string[];
 
 hobbies = ['sports', 'cooking'];
 
-let person: {
+type Person = {
   name: string,
   age: number,
 }; 
+
+// let person: {
+//   name: string,
+//   age: number,
+// }; 
+
+let person: Person;
 
 person = {
   name: 'Max',
@@ -45,6 +52,94 @@ let people: {
 
 // Type inference
 
-let course = "React - The Complete Guide";
+let course: string | number = "React - The Complete Guide";
 
-//course = 12323423;
+course = 12323423;
+
+// Functions & types
+
+function add(a:number, b:number){
+  return a + b;
+}
+
+
+// void, does not return anything.
+function printOutput(value: any){
+  console.log(value);
+}
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T){
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1,2,3];
+
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd')
+
+// class Student {
+//   firstName: string;
+//   lastName: string;
+//   age: number;
+//   courses: string[];
+
+//   constructor(first: string, last: string, age: number, courses: string[]){
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.courses = courses;
+//   }
+
+//   enroll(courseName: string){
+//     this.courses.push(courseName);
+//   }
+// }
+
+class Student {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public age: number,
+    private courses: string[]
+  ){}
+
+  enroll(courseName: string){
+    this.courses.push(courseName);
+  }
+  listCourses(){
+    return this.courses.slice();
+  }
+}
+
+const student = new Student('Max', 'Schwarz', 32, ['Angular']);
+student.enroll('React'); 
+
+
+interface Human {
+  firstName: string;
+  age: number;
+
+  greet: () => void;
+}
+
+let max: Human;
+
+max = {
+  firstName: 'Max',
+  age: 32,
+  greet() {
+      console.log('Hello!');
+  },
+}
+
+class Instructor implements Human{
+  firstName: string;
+  age: number;
+  greet(){
+    console.log('Hello!');
+  }
+}
